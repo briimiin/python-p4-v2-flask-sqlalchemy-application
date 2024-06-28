42db4fbead1d3d674f211f256072927d7f86c997
briimiin <kipkorirbrimin005@gmail.com>
2024-06-28 15:19:08 +0300

    sqlalchemy application

--- server/app.py
+++ server/app.py
@@ -11,41 +11,36 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-
+@app.route('/')
 @app.route('/')
 def index():
-    response = make_response(
-        '<h1>Welcome to the pet directory!</h1>',
-        200
-    )
-    return response
+    body = {'message': 'Welcome to the pet directory!'}
+    return make_response(body, 200)
 
+@app.route('/demo_json')
+def demo_json():
+    pet = Pet.query.first()
+    pet_dict = {'id': pet.id,
+                'name': pet.name,
+                'species': pet.species
+                }
+
+    return make_response(pet_dict, 200)
 @app.route('/pets/<int:id>')
 def pet_by_id(id):
     pet = Pet.query.filter(Pet.id == id).first()
 
     if pet:
-        response_body = f'<p>{pet.name} {pet.species}</p>'
-        response_status = 200
+        body = {'id': pet.id,
+                'name': pet.name,
+                'species': pet.species}
+        status = 200
     else:
-        response_body = f'<p>Pet {id} not found</p>'
-        response_status = 404
+        body = {'message': f'Pet {id} not found.'}
+        status = 404
 
-    response = make_response(response_body, response_status)
-    return response
-
-@app.route('/species/<string:species>')
-def pet_by_species(species):
-    pets = Pet.query.filter_by(species=species).all()
-
-    size = len(pets)  # all() returns a list so we can get length
-    response_body = f'<h2>There are {size} {species}s</h2>'
-    for pet in pets:
-        response_body += f'<p>{pet.name}</p>'
-    response = make_response(response_body, 200)
-    return response
+    return make_response(body, status)
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
